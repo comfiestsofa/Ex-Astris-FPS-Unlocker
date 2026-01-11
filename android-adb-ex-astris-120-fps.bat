129674f15eba9d4699bf43e04f3a604c71fb18b8
sofa <sophiapark0@proton.me>
2026-01-11 12:17:22 -0800

    auto find adb from android studio

--- android-adb-ex-astris-120-fps.bat
+++ android-adb-ex-astris-120-fps.bat
@@ -1,3 +1,4 @@
 @echo off
+set PATH=%PATH%;%USERPROFILE%\AppData\Local\Android\sdk\platform-tools
 adb shell "curl -fsSL https://raw.githubusercontent.com/comfiestsofa/Ex-Astris-FPS-Unlocker/refs/heads/master/android/ex-astris-120-fps.sh | sh"
 pause
