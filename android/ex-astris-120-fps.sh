00e3e884280cd9a9fc9a79ffe21c90cf24fec6f2
sofa <sophiapark0@proton.me>
2026-01-11 12:08:48 -0800

    make this easier

--- android/ex-astris-120-fps.sh
+++ android/ex-astris-120-fps.sh
@@ -1,3 +1,11 @@
-#/usr/bin/env bash
-sed -i 's/^\([[:space:]]*\)"frameRate":[[:space:]]*[0-9][0-9]*,/\1"frameRate": 120,/' "/storage/emulated/0/Android/data/com.gryphline.exastris.gp/files/GraphicOptionSaveSetting"
-cat "/storage/emulated/0/Android/data/com.gryphline.exastris.gp/files/GraphicOptionSaveSetting"
+#!/usr/bin/env bash
+EX_ASTRIS_GRAPHICS_SETTINGS="/storage/emulated/0/Android/data/com.gryphline.exastris.gp/files/GraphicOptionSaveSetting"
+if [[ -f "$EX_ASTRIS_GRAPHICS_SETTINGS" ]]; then
+	# Update frameRate to 120
+	sed -i 's/^\([[:space:]]*\)"frameRate":[[:space:]]*[0-9][0-9]*,/\1"frameRate": 120,/' "$EX_ASTRIS_GRAPHICS_SETTINGS"
+	
+	# Display the updated file
+	cat "$EX_ASTRIS_GRAPHICS_SETTINGS"
+else
+	echo "Please open Ex Astris and let it reach at least the title screen. (Error: Couldn't find $FILE)"
+fi
