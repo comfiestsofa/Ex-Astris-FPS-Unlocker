cca556c0f4bb883817361b98e71e2f714fe6a430
sofa <sophiapark0@proton.me>
2026-08-12 19:52:27 -0700

    don't rely on curl for android

--- android-adb-ex-astris-120-fps.bat
+++ android-adb-ex-astris-120-fps.bat
@@ -1,4 +1,19 @@
 @echo off
+
+:: Set PATH to include adb
 set PATH=%PATH%;%USERPROFILE%\AppData\Local\Android\sdk\platform-tools
-adb shell "curl -fsSL https://raw.githubusercontent.com/comfiestsofa/Ex-Astris-FPS-Unlocker/refs/heads/master/android/ex-astris-120-fps.sh | sh"
+
+:: Disabled because some Android devices (including MuMuPlayer on Android 15) don't have curl
+:: adb shell "curl -fsSL https://raw.githubusercontent.com/comfiestsofa/Ex-Astris-FPS-Unlocker/refs/heads/master/android/ex-astris-120-fps.sh | sh"
+
+:: Use PowerShell to base64 encode the script into a temp file
+powershell -Command "[Convert]::ToBase64String([IO.File]::ReadAllBytes('android\ex-astris-120-fps.sh'))" > "%TEMP%\ex-astris-120-fps.sh.base64"
+
+:: Read the base64 content into a variable
+set /p BASH_SCRIPT_BASE64= < "%TEMP%\ex-astris-120-fps.sh.base64"
+del /f "%TEMP%\ex-astris-120-fps.sh.base64"
+
+adb shell "echo \"%BASH_SCRIPT_BASE64%\" | base64 -d | sh"
+
 pause
+exit
--- android-adb-ex-astris-120-fps.sh
+++ android-adb-ex-astris-120-fps.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
+
+# cd to path of script
+cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")"
+
+# Set PATH to include adb
 PATH="$PATH:$USERPROFILE/AppData/Local/Android/sdk/platform-tools:${HOME}/Library/Android/sdk/platform-tools:${HOME}/Android/Sdk/platform-tools"
-adb shell "curl -fsSL https://raw.githubusercontent.com/comfiestsofa/Ex-Astris-FPS-Unlocker/refs/heads/master/android/ex-astris-120-fps.sh | sh"
+
+# Disabled because some Android devices (including MuMuPlayer on Android 15) don't have curl
+# adb shell "curl -fsSL https://raw.githubusercontent.com/comfiestsofa/Ex-Astris-FPS-Unlocker/refs/heads/master/android/ex-astris-120-fps.sh | sh"
+
+adb shell "echo \"$(cat "android/ex-astris-120-fps.sh" | base64)\" | base64 -d | sh"
+
+exit
