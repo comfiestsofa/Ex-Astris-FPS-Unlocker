9a4f2ecb2072fb17ddf81a31b1c4f81f0ee4622f
sofa <sophiapark0@proton.me>
2026-08-12 19:55:33 -0700

    prevent cmd exit, fix comment

--- android-adb-ex-astris-120-fps.bat
+++ android-adb-ex-astris-120-fps.bat
@@ -3,7 +3,7 @@
 :: Set PATH to include adb
 set PATH=%PATH%;%USERPROFILE%\AppData\Local\Android\sdk\platform-tools
 
-:: Disabled because some Android devices (including MuMuPlayer on Android 15) don't have curl
+:: Disabled because some Android installs (including MuMuPlayer Android 15 despite it existing on MuMuPlayer Android 12) don't have curl
 :: adb shell "curl -fsSL https://raw.githubusercontent.com/comfiestsofa/Ex-Astris-FPS-Unlocker/refs/heads/master/android/ex-astris-120-fps.sh | sh"
 
 :: Use PowerShell to base64 encode the script into a temp file
@@ -16,4 +16,3 @@ del /f "%TEMP%\ex-astris-120-fps.sh.base64"
 adb shell "echo \"%BASH_SCRIPT_BASE64%\" | base64 -d | sh"
 
 pause
-exit
--- android-adb-ex-astris-120-fps.sh
+++ android-adb-ex-astris-120-fps.sh
@@ -6,7 +6,7 @@ cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")"
 # Set PATH to include adb
 PATH="$PATH:$USERPROFILE/AppData/Local/Android/sdk/platform-tools:${HOME}/Library/Android/sdk/platform-tools:${HOME}/Android/Sdk/platform-tools"
 
-# Disabled because some Android devices (including MuMuPlayer on Android 15) don't have curl
+# Disabled because some Android installs (including MuMuPlayer Android 15 despite it existing on MuMuPlayer Android 12) don't have curl
 # adb shell "curl -fsSL https://raw.githubusercontent.com/comfiestsofa/Ex-Astris-FPS-Unlocker/refs/heads/master/android/ex-astris-120-fps.sh | sh"
 
 adb shell "echo \"$(cat "android/ex-astris-120-fps.sh" | base64)\" | base64 -d | sh"
